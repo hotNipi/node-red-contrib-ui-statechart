8962400f20cbe80ce6fed5ddd28cc31eaa362b9c
endele <endel.lepik@playtech.com>
2019-11-08 15:25:59 +0200

    main logic and layout

---
+++ statechart.js
@@ -0,0 +1,435 @@
+/*
+MIT License
+
+Copyright (c) 2019 hotNipi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+module.exports = function (RED) {
+	function HTML(config) {
+		var scopeconf = JSON.stringify(config.scope);	
+		var styles = String.raw`
+		<style>
+			.sc_txt-{{unique}} {	
+				font-size:`+config.fontoptions.normal+`em;			
+				fill: ${config.fontoptions.color};											
+			}
+			.sc_txt-{{unique}}.val{
+				font-size:`+config.fontoptions.extrasmall+`em;								
+			}						
+			.sc_txt-{{unique}}.small{
+				font-size:`+config.fontoptions.small+`em;
+			}
+			.scb-{{unique}}{
+				fill: ${config.onColor};
+				focusable="false";
+				outline: none !important;
+			}	
+			.scb-{{unique}}.curr{
+				filter: url(#brigth-{{unique}})
+			}
+			.scb-{{unique}}.off{
+				fill: ${config.offColor};				
+			}		
+		</style>`
+			
+		var layout = String.raw`
+					
+			<svg preserveAspectRatio="xMidYMid meet" id="statechart_svg_{{unique}}" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" ng-init='init(`+scopeconf+`)'>
+				<defs>
+				<filter id="brigth-{{unique}}">
+					<feComponentTransfer>
+					<feFuncR type="linear" slope="3"/>
+					<feFuncG type="linear" slope="3"/>
+					<feFuncB type="linear" slope="3"/>
+					</feComponentTransfer>
+					</filter>
+				</defs>
+				<g id="sc_{{unique}}_{{$index}}" ng-repeat="n in [].constructor(`+config.count+`) track by $index">
+					<rect class="scb-{{unique}}" id="scb_{{unique}}_{{$index}}" ng-attr-x="{{$index * `+config.scope.shape.step+`}}px" 
+						y="0"
+						width="`+config.scope.shape.width+`" 
+						height="0"
+						transform="scale(1,-1) translate(0,`+(config.scope.shape.height*-1)+`)" ng-click="onClick($index)">
+					</rect>
+					<text class="sc_txt-{{unique}} small" id=ser_{{unique}}_{{$index}} text-anchor="middle" dominant-baseline="baseline" 
+						ng-attr-x="{{$index * `+(config.scope.shape.step)+`}}px" dx="`+(config.scope.shape.width/2)+`" y=${config.exactheight}>
+					</text>
+					<text class="sc_txt-{{unique}} val" id=v_{{unique}}_{{$index}} text-anchor="middle" dominant-baseline="hanging"
+					ng-attr-x="{{$index * `+(config.scope.shape.step)+`}}px" dx="`+(config.scope.shape.width/2)+`" y=${config.exactheight}> 
+					</text>															
+				</g>
+				
+				
+			</svg>`			
+		
+		return String.raw`${styles}${layout}`;
+	}
+
+	function checkConfig(node, conf) {
+		if (!conf || !conf.hasOwnProperty("group")) {
+			node.error(RED._("ui_statechart.error.no-group"));
+			return false;
+		}
+		return true;
+	}
+
+	var ui = undefined;
+
+	function StateChartNode(config) {
+		try {
+			var node = this;			
+			if (ui === undefined) {
+				ui = RED.require("node-red-dashboard")(RED);
+			}			
+			RED.nodes.createNode(this, config);			
+			var done = null;
+			var range = null;
+			var site = null;		
+			var ensureNumber = null;
+			var getSiteProperties = null;
+			var getPosition = null;
+			var getCount = null;
+			var calculateShape = null;
+			var updateData = null;
+			var calcualteValue = null;
+			var formatSeries = null;
+			
+			
+			if (checkConfig(node, config)) {				
+				ensureNumber = function (input) {
+					if (input === undefined) {
+						return config.min;
+					}
+					if (typeof input !== "number") {
+						var inputString = input.toString();
+						input =  parseFloat(inputString)
+						if(isNaN(input)){
+							node.warn("msg.payload does not contain numeric value")
+							return config.min
+						}						
+					}
+					if(isNaN(input)){
+						node.warn("msg.payload does not contain numeric value")
+						input = config.min;
+					}					
+					return input;
+				}
+				getSiteProperties = function(){
+					var opts = null;					
+					if (typeof ui.getSizes === "function") {			
+						opts = {};
+						opts.sizes = ui.getSizes();
+						opts.theme = ui.getTheme();
+					}	
+					if(opts === null){
+						node.log("Couldn't reach to the site parameters. Using hardcoded default parameters!")
+						opts = {}
+						opts.sizes = { sx: 48, sy: 48, gx: 4, gy: 4, cx: 4, cy: 4, px: 4, py: 4 }
+						opts.theme = {'widget-backgroundColor':{value:"#097479"}}						
+					}									
+					return opts
+				}
+				range = function (n,p,r){					
+					var divisor = p.maxin - p.minin;							
+					n = n > p.maxin ? p.maxin - 0.00001 : n;
+					n = n < p.minin ? p.minin : n;
+					n = ((n - p.minin) % divisor + divisor) % divisor + p.minin;
+					n = ((n - p.minin) / (p.maxin - p.minin) * (p.maxout - p.minout)) + p.minout;										
+					if(!r){
+						return Math.round(n);
+					}				
+					return n					
+				}	
+				getPosition = function(target,min,max){
+					var p =  {minin:min, maxin:max+0.00001, minout:config.exactheight, maxout:1}
+					return range(target,p,true)
+				}
+				
+				getCount = function(){				
+					return config.scope.series.length
+				}
+				
+				calculateShape = function(){
+					var ret = {}
+					var gap = 2
+					var reserved = 10
+					var gaps = (config.count - 1) * gap
+					var total = config.exactwidth - gaps;
+					var one = total/config.count;
+					ret.width = one;
+					ret.step = one + gap;
+					ret.height = config.exactheight - reserved
+					return ret					
+				}
+				
+				calcualteValue = function(val){
+					var p =  {minin:config.min, maxin:config.max+0.00001, minout:3, maxout: config.scope.shape.height}
+					return range(val,p,true)
+				}
+			
+				updateData = function(values){
+					var i
+					var ob
+					if(!values){						
+						for (i = 0; i < config.count; i++) {
+							ob = {}
+							ob.value = 0													
+							ob.height = calcualteValue(0)
+							ob.state = true
+							config.data[config.scope.series[i]] = ob
+						}
+					}
+					else{											
+						var len = values.length
+						if(len > config.count){
+							len = config.count
+						}
+						for (i = 0; i < len; i++) {
+							if(config.scope.series.indexOf(values[i].series) == -1){
+								node.warn('Series "'+values[i].series+'" does not exist. Check your input!')
+								continue
+							}							
+							config.data[values[i].series].state = values[i].state
+							config.data[values[i].series].value = values[i].value										
+						}
+						config.min = Number.MAX_VALUE
+						config.max = Number.MIN_VALUE					
+						for (i = 0; i < config.count; i++) {
+							ob = config.data[config.scope.series[i]].value							
+							if(ob > config.max){
+								config.max = ob
+							}
+							if(ob < config.min){
+								config.min = ob
+							}
+						}
+						var ret = []
+						for (i = 0; i < config.count; i++) {
+							ob = config.data[config.scope.series[i]]
+							ob.height = calcualteValue(ob.value)
+							ret.push({value:ob.value,height:ob.height,state:ob.state})																							
+						}		
+					}
+					return ret					
+				}
+				
+				var formatSeries = function(ser){					
+					return ser.split(',')
+				}
+				
+				var group = RED.nodes.getNode(config.group);
+				var site = getSiteProperties();				
+				if(config.width == 0){ config.width = parseInt(group.config.width) || 6}
+				if(config.height == 0) {config.height = parseInt(group.config.height) || 2}
+				config.scope = {}
+				config.width = parseInt(config.width)
+				config.height = parseInt(config.height)
+				config.exactwidth = parseInt(site.sizes.sx * config.width + site.sizes.cx * (config.width-1)) - 12;		
+				config.exactheight = parseInt(site.sizes.sy * config.height + site.sizes.cy * (config.height-1)) - 12;
+				config.series = 'A,B,C,D,E,F,G,H,I,J,K,L,M'
+				config.scope.series = formatSeries(config.series)
+				config.count = getCount()				
+				config.scope.shape = calculateShape()
+				config.data = []				
+				config.fontoptions = {normal:1,small:0.6,extrasmall:0.5,color:'currentColor'}			
+				config.onColor = site.theme['widget-backgroundColor'].value	
+				config.offColor = 'gray'			
+				if(config.colorFromTheme == false){
+					config.onColor = config.colorON
+					config.offColor = config.colorOFF
+				}								
+				config.min = Number.MAX_VALUE
+				config.max = Number.MIN_VALUE
+				updateData()				
+				
+				var html = HTML(config);		
+				
+				done = ui.addWidget({
+					node: node,
+					order: config.order, 
+					group: config.group,
+					width: config.width,
+					height: config.height,									
+					format: html,					
+					templateScope: "local",
+					emitOnlyNewValues: false,
+					forwardInputMessages: true,
+					storeFrontEndInputAsState: true,				
+					
+					beforeEmit: function (msg) {
+						if(msg === undefined){
+							return 
+						}
+						if(msg.payload && msg.payload.length > 0){
+							msg.payload = updateData(msg.payload)						
+						}						
+						return { msg: msg };
+					},
+					
+					initController: function ($scope) {																		
+						$scope.unique = $scope.$eval('$id')
+						$scope.time = undefined
+						$scope.data = undefined	
+						$scope.series = undefined
+						$scope.init = function(config){
+							$scope.shape = config.shape
+							$scope.series = config.series						
+							pollInit()
+						}
+						var pollInit = function(){
+							var stateCheck = setInterval(function() {
+								if (updateSeries() == true) {
+									clearInterval(stateCheck);
+									clearValues()
+									updateBars()												
+								}
+							}, 40);
+						}
+						
+						var updateSeries = function (){							
+							var target
+							var found = false							
+							for (let i = 0; i < $scope.series.length; i++) {
+								target = document.getElementById("ser_"+$scope.unique+"_"+i);								
+								if(target){
+									$(target).text($scope.series[i])
+									$scope.inited = true
+									found = true
+								}
+							}
+							return found
+						}
+						
+						
+						var updateTime = function (t){
+							if(t == $scope.time){
+								return
+							}
+							var c
+							var line = document.getElementById("scb_"+$scope.unique+"_"+$scope.time);							
+							if(line){
+								c = line.getAttribute('class').replace(' curr','')																																			
+								line.setAttribute("class", c );								
+							}	
+							$scope.time = t							
+							line = document.getElementById("scb_"+$scope.unique+"_"+$scope.time);														
+							if(line){	
+								c = line.getAttribute('class') + ' curr'												
+								line.setAttribute("class",  c);
+							}	
+						}
+						var updateBars = function (data){							
+							if(data){
+								$scope.data = data
+							}
+							if($scope.series == undefined){
+								return
+							}
+							if($scope.data == undefined){
+								return
+							}																					
+							var c
+							var sci
+							var len = $scope.series.length
+							for (let i = 0; i < len; i++) {								
+								var line = document.getElementById("scb_"+$scope.unique+"_"+i);
+								if(line){									
+									sci = line.getAttribute('class').indexOf('off')
+									if(sci == -1 && $scope.data[i].state == false){										
+										c = line.getAttribute('class') + ' off'												
+										line.setAttribute("class",  c);										
+									}
+									else if(sci != -1 && $scope.data[i].state == true){
+										c = line.getAttribute('class').replace(' off','')																																			
+										line.setAttribute("class", c );			
+									}
+									line.setAttribute('height',$scope.data[i].height)					
+								}															
+							}
+							updateValues()
+						}
+						var clearValues = function (){
+							var len = $scope.series.length
+							var target
+							for (let i = 0; i < len; i++) {	
+								target = document.getElementById("v_"+$scope.unique+"_"+i);
+								if(target){								
+									$(target).text('')
+								}
+							}							
+						}
+						var updateValues = function (){							
+							var len = $scope.series.length
+							var target
+							var yp
+							var tl
+							var d
+							for (let i = 0; i < len; i++) {	
+								target = document.getElementById("v_"+$scope.unique+"_"+i);
+								if(target){																		
+									yp = $scope.shape.height - $scope.data[i].height - 10 	
+									if(yp <= 0){
+										yp = $scope.shape.height - $scope.data[i].height + 1
+									}									
+									d = $scope.data[i].value
+									tl = Math.ceil(Math.log(d + 1) / Math.LN10);							
+									if(tl > 3){
+										d = parseFloat(d/1000).toFixed(1)+'k'
+									}
+									else{
+										d = d.toFixed(3-tl)
+									}													
+									target.setAttributeNS(null,'y',yp)							
+									$(target).text(d)
+								}
+							}							
+						}						
+						$scope.onClick = function(id){
+							if($scope.data === undefined){
+								return
+							}							
+						}
+														
+						$scope.$watch('msg', function (msg) {
+							if (!msg) {								
+								return;
+							}
+							if(msg.payload){								
+								updateBars(msg.payload)
+							}
+							//updateTime(new Date().getHours())				
+																										
+						});
+					}
+				});
+			}
+		}
+		catch (e) {
+			console.log(e);
+		}
+		node.on("close", function () {
+			if (done) {
+				done();
+			}
+		});
+	}
+	RED.nodes.registerType("ui_statechart", StateChartNode);
+};
